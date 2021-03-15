2a8c7c5cfd4cb82649e21527a0f436479a2240e8
alich <alichagher88@gmail.com>
2021-03-15 12:59:02 +0330

    connecting in database

--- config.php
+++ config.php
@@ -1,4 +1,27 @@
 <?php
-//mysqli_connect("localhost","root","");
-//mysqli_select_db("users");
-$connection=mysqli_connect("localhost","root","","users");
+//$connection=mysqli_connect("localhost","root","","users");
+
+class DB{
+    private $servername='localhost';
+    private $dbname='users';
+    private $dbusername='root';
+    private $dbpassword='';
+    public $connection;
+            function __construct() {
+            try {
+                $this->connection=new PDO("mysql:host=$this->servername;dbname=$this->dbname",
+                $this->dbusername, $this->dbpassword);
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connection->exec('SET NAMES "utf8"');
+                return $this->connection;
+
+                
+                
+            } catch (PDOException $e) {
+                echo 'error in connection:'.$e->getMessage();
+                exit();
+ 
+        }
+    }
+}
+
